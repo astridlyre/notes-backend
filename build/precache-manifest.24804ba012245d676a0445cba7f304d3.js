b58491bd3705e014da97b7f6fd68a8bb85413e2b
unknown <astrid.lyre@outlook.com>
2020-08-29 07:31:55 -0400

    Added frontend code

---
+++ build/precache-manifest.24804ba012245d676a0445cba7f304d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6fa24eff72460497814bf08905e086d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d40c757f7a947c86bcdd",
+    "url": "/static/css/main.d99330e2.chunk.css"
+  },
+  {
+    "revision": "c328b32efbda2cf23f49",
+    "url": "/static/js/2.2ca4f5a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2ca4f5a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d40c757f7a947c86bcdd",
+    "url": "/static/js/main.88252d30.chunk.js"
+  },
+  {
+    "revision": "cbf25fd7ab88aa36116a",
+    "url": "/static/js/runtime-main.ba9ebe47.js"
+  }
+]);
